4ed0550b69c7787bd949b08bd6bc204481e225ec
Steve Calvert <scalvert@mobify.com>
2014-07-03 14:46:37 -0700

    Reworking event tests

--- tests/unit/bellows_events.js
+++ tests/unit/bellows_events.js
@@ -12,18 +12,50 @@ define([
             element = $(fixture);
         });
 
-        it('fires the open event when the header is clicked', function() {
-            var triggered = false;
-
+        it('fires the open event when the header is clicked', function(done) {
             element.bellows({
                 open: function() {
-                    triggered = true;
+                    done();
                 }
             });
 
             element.find('.bellows__header').first().trigger('tap');
+        });
 
-            assert.isTrue(triggered);
+        it('fires the opened event when the header is clicked', function(done) {
+            element.bellows({
+                opened: function() {
+                    done();
+                }
+            });
+
+            element.find('.bellows__header').first().trigger('tap');
+        });
+
+        it('fires the close event when the header is clicked', function(done) {
+            element.bellows({
+                opened: function(e, ui) {
+                    ui.item.find('.bellows__header').trigger('tap');
+                },
+                close: function() {
+                    done();
+                }
+            });
+
+            element.find('.bellows__header').first().trigger('tap');
+        });
+
+        it('fires the closed event when the header is clicked', function(done) {
+            element.bellows({
+                opened: function(e, ui) {
+                    ui.item.find('.bellows__header').trigger('tap');
+                },
+                closed: function() {
+                    done();
+                }
+            });
+
+            element.find('.bellows__header').first().trigger('tap');
         });
     });
 });
