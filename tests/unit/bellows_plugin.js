7e8d4b54926a333ca5cda6003cf5968fc6c2cc07
Steve Calvert <scalvert@mobify.com>
2014-07-03 09:42:30 -0700

    Adding some plugin tests

--- tests/unit/bellows_plugin.js
+++ tests/unit/bellows_plugin.js
@@ -1,24 +1,46 @@
 define([
     'zepto',
     'bellows'
-],function($) {
+], function($) {
     var element;
 
     describe('Bellows plugin', function() {
         beforeEach(function() {
-            element = $('<div class="bellows"></div>');
+            element = $('<div class="bellows default"> <div class="bellows__item"> <div class="bellows__header"><h3>Header</h3></div> <div class="bellows__content"> <p>Content</p> </div> </div> <div class="bellows__item"> <div class="bellows__header"> <h3>Header</h3> </div> <div class="bellows__content"> <p>Content</p> </div> </div> </div>');
         });
 
-        it('attaches bellows to Zepto\'s fn', function() {
-            var bellows = $.fn.bellows;
+        describe('binding to Zepto\'s fn', function() {
+            it('defines Bellows in Zepto', function() {
+                var bellows = $.fn.bellows;
 
-            assert.isDefined(bellows);
+                assert.isDefined(bellows);
+            });
+
+            it('defines bellows as a function', function() {
+                var bellows = $.fn.bellows;
+
+                assert.isFunction(bellows);
+            });
         });
 
-        it('defines bellows as a function', function() {
-            var bellows = $.fn.bellows;
+        describe('invoking bellows', function() {
+            it('creates bellows instance on element', function() {
+                element.bellows();
 
-            assert.isFunction(bellows);
+                assert.isDefined(element.data('bellows'));
+            });
+
+            it('stores element inside instance', function() {
+                element.bellows();
+
+                assert.isDefined(element.data('bellows').$bellows);
+            });
+
+            it('wraps each content section', function() {
+                element.bellows();
+
+                assert.lengthOf(element.find('.bellows__content-wrapper'), 2);
+            });
         });
     });
 });
