b0514a46e4978221c770be4ec9eea654a3192a49
Ted Tate <tedtate@gmail.com>
2014-08-26 12:37:53 -0700

    Adding example of bellows usage without require

--- examples/assets/js/main.js
+++ examples/assets/js/main.js
@@ -1,27 +1,9 @@
 require(['config'], function() {
     require([
         '$',
-        'bellows'
+        'setup-bellows'
     ],
-    function($) {
-        // Default bellows initialization (/examples/default.html)
-        $('.bellows.default').bellows();
-
-        // Bellows with custom animation duration and easing (/examples/animation.html)
-        $('.bellows.animation').bellows({
-            duration: 1000,
-            easing: "easeInOutCubic"
-        });
-
-        // Bellows within bellows within bellows... (/examples/inception.html)
-        $('.bellows.inception, .bellows.inception .bellows').bellows();
-
-        // Bellows with single item open (/examples/single.html)
-        $('.bellows.single').bellows({
-            singleItemOpen: true
-        });
-
-        // Enable active states
-        $(document).on('touchstart', function() {});
+    function($, setupBellows) {
+        setupBellows.init();
     });
 });
