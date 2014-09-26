a5173ca3caa73179fdfc0ab02e68652de2a60884
Steve Calvert <scalvert@mobify.com>
2014-09-26 10:25:59 -0700

    Changing bellows to use mobify velocity

--- tests/gremlins/config.js
+++ tests/gremlins/config.js
@@ -4,7 +4,7 @@ require.config({
         'text': 'bower_components/requirejs-text/text',
         'gremlins': 'bower_components/gremlins.js/gremlins.min',
         '$': 'lib/zeptojs/dist/zepto.min',
-        'velocity': 'bower_components/velocity/velocity',
+        'velocity': 'bower_components/mobify-velocity/velocity',
         'bellows': 'dist/bellows'
     },
     'shim': {
