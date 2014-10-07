e500dc254c56a46125d2e02033dc48e4cad3e20c
Steve Calvert <scalvert@mobify.com>
2014-10-06 20:54:37 -0700

    Updating version of velocity

--- dist/bellows.js
+++ dist/bellows.js
@@ -29,7 +29,7 @@
         Bellows.__super__.call(this, element, options, Bellows.DEFAULTS);
     }
 
-    Bellows.VERSION = '4.1.0';
+    Bellows.VERSION = '4.1.1';
 
     Bellows.DEFAULTS = {
         singleItemOpen: false,
